6b244b86550559e23d015ea9c9507e4978e82211
aheaton <Pappy123!>
2017-10-19 21:04:48 -0400

    Finish create round functionality in UI

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -43,7 +43,7 @@ class RoundsController < ProtectedController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_round
-      @round = Round.find(params[:id])
+      @round = current_user.rounds.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
--- scripts/rounds_with_auth/index.sh
+++ scripts/rounds_with_auth/index.sh
@@ -1,3 +1,5 @@
 curl --include --request GET "http://localhost:4741/rounds"
+--header "Content-Type: application/json" \
+--header "Authorization: Token token=$TOKEN"
 
-# run in bash: sh scripts/rounds/index.sh
+# run in bash: TOKEN='BAhJIiU5MWVhNDk1NGYyMDk5Yzc2ZDI1ODA2MWNhNTI2YmE5YgY6BkVG--427f6aa27f0bd30df11406ac9378568360e1c583' sh scripts/rounds_with_auth/index.sh
